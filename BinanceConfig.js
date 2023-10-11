5687037f20655751368e10979b14f81822a936f1
dkb-development <80267630+dkb-development@users.noreply.github.com>
2023-10-11 09:33:55 +0530

    Add files via upload

---
+++ BinanceConfig.js
@@ -0,0 +1,8 @@
+import Binance from 'node-binance-api';
+export const binance = new Binance().options({
+  APIKEY: 'R4bRiULdDcIs0aOgh7Np9jttPEgmmbqXGFbh8Zvhf5gw088aMZagSAAHmV9bSmVE',
+  APISECRET: 'zhliJWqsCV38VZDO9YwG4VFjFgBsHvjDtGSs37mGKKVwZDANfTZvXn8kcPnJDhje'
+});
+
+export const BinanceApiKey = 'R4bRiULdDcIs0aOgh7Np9jttPEgmmbqXGFbh8Zvhf5gw088aMZagSAAHmV9bSmVE';
+export const BinanceApiSecret = 'zhliJWqsCV38VZDO9YwG4VFjFgBsHvjDtGSs37mGKKVwZDANfTZvXn8kcPnJDhje';
